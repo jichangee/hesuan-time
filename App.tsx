9b74a9fb96d133f75d5e45a02d1dd1e30bfa5fec
jichangee <jichangee@gmail.com>
2022-06-28 16:04:32 +0800

    2 files modified

--- App.tsx
+++ App.tsx
@@ -1,19 +1,21 @@
 import * as React from 'react';
 import dayjs from 'dayjs';
+import { Button, DatePicker, Space } from 'antd';
 import './style.css';
 
 const DATE_FORMAT = 'YYYY-MM-DD HH:mm:ss';
 
 export default function App() {
-  const [currentDate, setCurrentDate] = React.useState('');
-  const [intervalHours, setIntervalHours] = React.useState(48);
-  const [actionType, setActionType] = React.useState(2); //核酸采集时间 0 - 每天  1 - 单号  2 - 双号
+  const [currentDate, setCurrentDate] = React.useState();
+  const [intervalHours, setIntervalHours] = React.useState(24 * 7);
+  const [actionType, setActionType] = React.useState(2); // 核酸采集时间 0 - 每天  1 - 单号  2 - 双号
   const [nextDate, setNextDate] = React.useState('');
-  const getNextDate = (current = '') => {
+  const getNextDate = (current) => {
     let $d = dayjs(current).add(intervalHours, 'h');
     if (actionType === 0) {
       return $d.format(DATE_FORMAT);
     }
+    console.log('$d', $d.format(DATE_FORMAT));
     $d = getValidate($d, actionType === 1);
     return $d.format(DATE_FORMAT);
   };
@@ -27,23 +29,19 @@ export default function App() {
   const isOdd = ($d) => {
     return +$d.format('D') % 2 === 1;
   };
-  const handleConfirm = () => {
-    setNextDate(getNextDate(currentDate));
+  const onDateChange = (m, value) => {
+    console.log('value', value);
+    setCurrentDate(value);
   };
   React.useEffect(() => {
-    handleConfirm();
-  }, []);
+    setNextDate(getNextDate(currentDate));
+  }, [currentDate]);
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-      <h2>{nextDate}</h2>
-      <input
-        value={currentDate}
-        onChange={(e) => setCurrentDate(e.target.value)}
-        type="datetime-local"
-      />
-      <button onClick={handleConfirm}>确认</button>
+    <div className="main">
+      <h2>Next Time: {nextDate}</h2>
+      <Space>
+        <DatePicker format={DATE_FORMAT} showTime onChange={onDateChange} />
+      </Space>
     </div>
   );
 }
